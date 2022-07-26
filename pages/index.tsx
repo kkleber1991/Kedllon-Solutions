4acf5c79219adcbe82630d9e2158a9baf552d30f
kkleber1991 <kkleber.1991@gmail.com>
2022-07-26 19:08:45 -0300

    Lançamento
    Lançamento do projeto!

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <Container maxW='full' bg='#000'>
+    </Container>
+  )
+}
+
+export default Home
